150f0aba9bc9c51bd5d14993b3276ecd6e499214
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-06-04 11:10:23 -0300

    feat: Implementado filtro de pesquisa

--- frontend/tickets/views.py
+++ frontend/tickets/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from tickets.models import Ticket
 
 
@@ -9,6 +10,19 @@ class TicketListView(ListView):
     model = Ticket
     template_name = 'tickets/ticket_list.html'
     context_object_name = 'tickets'
+
+    def get_queryset(self):
+        """
+        Retorna os ingressos pelo filtro
+        """
+        search = self.request.GET.get('search', '')
+
+        # Se houver busca, filtra os ingressos pelo nome
+        if search:
+            return Ticket.objects.filter(name__icontains=search).order_by('-created_at')
+
+        # Caso contrário, retorna todos os ingressos
+        return Ticket.objects.all()
 
 
 class TicketDetailView(DetailView):
