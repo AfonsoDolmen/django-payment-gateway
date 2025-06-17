03b00e50d1a5cfe49b2a3590c29cfd45bced4721
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-06-17 15:53:43 -0300

    hotfix: Ajustes

--- frontend/payments/admin.py
+++ frontend/payments/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from payments.models import Payment
 
-
-class PaymentAdmin(admin.ModelAdmin):
-    class Meta:
-        list_filter = ('id', 'status', 'total_amount', 'total_paid_amount',)
-
-
-admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Payment)
